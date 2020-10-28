a97583928cc6543793a8f7b902c82982e3f88930
Corey Melnick <cmelnick@bnl.gov>
2020-10-28 11:59:11 -0400

    Library version

---
+++ lib/libCTQMC.C
@@ -0,0 +1,64 @@
+#include "libCTQMC.h"
+
+ut::Beta ut::beta;
+
+extern "C" int EvalSim_Main(const char* case_name)
+{
+    try {
+        evalsim::evalsim_driver(case_name);
+    }
+    catch(std::exception& exc) {
+        std::cerr << exc.what() << "( Thrown from worker " << mpi::rank() << " )" << std::endl;
+           
+        return -1;
+    }
+    catch(...) {
+        std::cerr << "Fatal Error: Unknown Exception! ( Thrown from worker " << mpi::rank() << " )" << std::endl;
+           
+        return -2;
+    }
+       
+    return 0;
+}
+
+#ifdef MAKE_GPU_ENABLED
+extern "C" int CTQMCDriverStart(const char* case_name)
+{
+        try {
+            ctqmc::ctqmc_driver(case_name);
+        }
+        catch (std::exception& exc) {
+            std::cerr << exc.what() << " ( Thrown from worker " << mpi::rank() << " )" << std::endl;
+
+            return -1;
+        }
+        catch (...) {
+            std::cerr << "Fatal Error: Unknown Exception! ( Thrown from worker " << mpi::rank() << " )" << std::endl;
+
+            return -2;
+        }
+        
+    return 0;
+}
+#else
+extern "C" int CTQMCDriverStart(const char* case_name)
+{
+        try {
+            ctqmc::ctqmc_driver(case_name);
+        }
+        catch (std::exception& exc) {
+            std::cerr << exc.what() << " ( Thrown from worker " << mpi::rank() << " )" << std::endl;
+
+            return -1;
+        }
+        catch (...) {
+            std::cerr << "Fatal Error: Unknown Exception! ( Thrown from worker " << mpi::rank() << " )" << std::endl;
+
+            return -2;
+        }
+        
+    return 0;
+}
+#endif
+
+
