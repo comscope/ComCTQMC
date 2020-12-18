a0beb8d368157ca0c5178ff44dba9f9f81570f69
Corey Melnick <cmelnick@bnl.gov>
2020-12-18 14:56:53 -0500

    GPU variance

--- ctqmc/device/planar_complex/ctqmc.C
+++ ctqmc/device/planar_complex/ctqmc.C
@@ -103,6 +103,7 @@ namespace ctqmc{
     
     
     if(jSimulation.is("error")) mpi::write(jSimulation("error"), std::string(case_name) + ".err.json");
+    if(jSimulation.is("variance")) mpi::write(jSimulation("variance"), std::string(std::string(case_name)) + ".var.json");
     if(jSimulation.is("resample")) jsx::write(jSimulation("resample"), std::string(case_name) + ".meas" + std::to_string(mpi::rank()) + ".json");
     
     
